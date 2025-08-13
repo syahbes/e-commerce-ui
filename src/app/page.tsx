de262babecd0672bd986d92f0d43e010ce5be9f8
Shlomo Y <shlomo@techtio.io>
2025-08-13 16:52:51 +0300

    card and color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
 
-export default Homepage
+const Homepage = () => {
+    return (
+        <div className="">
+            <div className="relative aspect-[3/1] mb-12">
+                <Image src="/featured.png" alt="Featured Product" fill 
+                />
+            </div>
+            <ProductList />
+        </div>
+    );
+};
+
+export default Homepage;
